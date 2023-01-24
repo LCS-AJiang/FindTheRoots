6ba8e1d2f2693501a3ba80eb67a7540d37b8936b
Audrey Jiang <ajiang@lcs.on.ca>
2023-01-24 08:53:41 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Audrey Jiang on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
